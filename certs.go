bc2492bc704c4d1440c4b6ec4241e8a947cf39b8
Jack Horgan <tsnm61tt@duck.com>
2026-02-24 22:28:21 -0500

    fix private key parsing in GetLeaf

--- certs.go
+++ certs.go
@@ -1,31 +1,3 @@
-// This is free and unencumbered software released into the public domain.
-
-// Anyone is free to copy, modify, publish, use, compile, sell, or
-// distribute this software, either in source code form or as a compiled
-// binary, for any purpose, commercial or non-commercial, and by any
-// means.
-
-// In jurisdictions that recognize copyright laws, the author or authors
-// of this software dedicate any and all copyright interest in the
-// software to the public domain. We make this dedication for the benefit
-// of the public at large and to the detriment of our heirs and
-// successors. We intend this dedication to be an overt act of
-// relinquishment in perpetuity of all present and future rights to this
-// software under copyright law.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-// OTHER DEALINGS IN THE SOFTWARE.
-
-// For more information, please refer to <https://unlicense.org>
-
-// Code adapted from https://gist.github.com/shaneutt/5e1995295cff6721c89a71d13a71c251#file-ca_and_cert_golang_demo-go
-
-// Big thanks to Shane Utt! :)
 
 package scamjamtls
 
@@ -131,7 +103,7 @@ func GetLeaf(logger *zerolog.Logger, certOrgName string, caCertAsBytes []byte, c
 		return nil, nil, err
     }
 
-	caPrivateKey, err := x509.ParseCertificate(caPrivateKeyPEM.Bytes)
+	caPrivateKey, err := x509.ParsePKCS1PrivateKey(caPrivateKeyPEM.Bytes)
 	if err != nil {
 		logger.Error().Err(err).Msg("Failed to decode root CA private key from PEM!")
 		return nil, nil, err
